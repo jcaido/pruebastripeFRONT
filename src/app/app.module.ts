7165636998a1e0cda836a4077036452dfcef67b2
Cristobal Rosa <crisrogar@gmail.com>
2024-07-23 18:59:27 +0200

    create payment component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ModalComponent } from './modal/modal.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ModalComponent
+    ModalComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
