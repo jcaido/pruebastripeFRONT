3c81869544aa94b2a1a9c676ca4a261650e2733e
Cristobal Rosa <crisrogar@gmail.com>
2024-07-24 12:05:06 +0200

    update routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListArticleComponent } from './article/list-article.component';
+import { DetailArticleComponent } from './article/detail-article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListArticleComponent},
+  {path: 'list', component: ListArticleComponent},
+  {path: 'detail/:id', component: DetailArticleComponent},
+  {path: '**', redirectTo: 'list', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
