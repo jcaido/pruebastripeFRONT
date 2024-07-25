49b1dd730f8d9b03ee9c1ea33f013ceecf5b8b9b
Cristobal Rosa <crisrogar@gmail.com>
2024-07-25 23:25:03 +0200

    update payment component

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
+import { enviroment } from '../../enviroments/enviroment';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
+
+  stripe: any;
+  card: any;
+
+  constructor() { }
+
+  async ngOnInit() {
+    this.stripe = await loadStripe(enviroment.publicAPIKey);
+
+    const elements = this.stripe.elements();
+    this.card = elements.create('card');
+    this.card.mount('#card-element');
+  }
+
+  handlePayment() {
+
+  }
 
 }
