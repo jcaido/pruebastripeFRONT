751b115c376e25579d9ed7f74d5c7f6cbb36dbc0
Cristobal Rosa <crisrogar@gmail.com>
2024-07-30 10:38:02 +0200

    create method list_articles in article service

--- src/app/article/article.service.ts
+++ src/app/article/article.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from '../model/article';
+
+const ARTICLE_API = 'http://localhost:8080/api/article/';
+
+const httOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public list_articles(): Observable<Article[]> {
+    return this.httpClient.get<Article[]>(ARTICLE_API + "articles", httOptions);
+  }
+
 }
