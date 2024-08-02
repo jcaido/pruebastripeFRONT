eaab154596b56827ea8e0a3b593e5df10773434c
Cristobal Rosa <crisrogar@gmail.com>
2024-08-02 19:50:06 +0200

    create method confirm in payment service

--- src/app/payment/payment.service.ts
+++ src/app/payment/payment.service.ts
@@ -1,4 +1,11 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const STRIPE_API = 'http://localhost:8080/api/stripe/';
+
+const httOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+}
 
 @Injectable({
   providedIn: 'root'
