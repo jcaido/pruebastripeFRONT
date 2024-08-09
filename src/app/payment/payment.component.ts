626fa594392c680874c12ac6602f4be816361bb5
Cristobal Rosa <crisrogar@gmail.com>
2024-08-09 12:11:32 +0200

    update payment component

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,29 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { loadStripe } from '@stripe/stripe-js';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
+import { FormBuilder, Validators } from '@angular/forms';
+import { injectStripe } from 'ngx-stripe';
 import { enviroment } from '../../enviroments/enviroment';
+import { StripeCardElementOptions, StripeElementsOptions } from '@stripe/stripe-js';
+
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
-export class PaymentComponent implements OnInit {
+export class PaymentComponent {
 
-  stripe: any;
-  card: any;
+  @Input() id: any;
+  @Input() price: any;
+  @Input() description: any;
+  @Input() name: any;
 
-  constructor() { }
+  error: any;
 
-  async ngOnInit() {
-    this.stripe = await loadStripe(enviroment.publicAPIKey);
+  readonly dialog = inject(MatDialog);
 
-    const elements = this.stripe.elements();
-    this.card = elements.create('card');
-    this.card.mount('#card-element');
-  }
+  constructor(private fb: FormBuilder) { }
 
-  handlePayment() {
+  cardOptions: StripeCardElementOptions = {
+    style: {
+      base: {
+        iconColor: '#666EE8',
+        color: '#31325F',
+        fontWeight: '300',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSize: '18px',
+        '::placeholder': {
+          color: '#CFD7E0'
+        }
+      }
+    }
+  };
 
+  elementsOptions: StripeElementsOptions = {
+    locale: 'es'
+  };
+
+  checkOutForm = this.fb.group({
+    name: ['', Validators.required]
+  })
+
+  stripe = injectStripe(enviroment.publicAPIKey);
+
+  openModalPayment(): void {
+      const dialogRef = this.dialog.open(ModalComponent, {
+        data: {id: this.id, price: this.price, description: this.description, name: this.name}
+      });
+      console.log(this.id);
   }
 
 }
